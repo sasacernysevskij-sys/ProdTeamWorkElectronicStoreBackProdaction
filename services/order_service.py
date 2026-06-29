e3f4cc9753812d491c28c3af4f14696f543d8b02
sasacernysevskij-sys <sasacernysevskij@gmail.com>
2026-06-29 16:44:22 +0300

    commit12
    des

--- services/order_service.py
+++ services/order_service.py
@@ -127,13 +127,13 @@ class OrderService:
             db.refresh(order)
 
             # Отправляем письмо
-            self.send_order_email(
-                db=db,
-                user_id=user_id,
-                order_id=order.id,
-                total_price=total_price,
-                items=order_items_response
-            )
+            # self.send_order_email(
+            #     db=db,
+            #     user_id=user_id,
+            #     order_id=order.id,
+            #     total_price=total_price,
+            #     items=order_items_response
+            # )
 
             return {
                 "message": "Заказ успешно создан",
